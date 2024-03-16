f739b7a95b0e880ead05f99c96b6ae4b67915374
youtlane <youtlanehanane@gmail.com>
2024-03-16 15:36:35 +0100

    fix: [Bug Hunt] - Dashboard pourvoir déplier plusieurs listes, et consulter les tickets de chacune des deux listes.

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -145,11 +145,13 @@ export default class {
       this.counter ++
     }
 
-    bills.forEach(bill => {
+    const filteredBillsValue = filteredBills(bills, getStatus(this.index));    
+
+    filteredBillsValue.forEach(bill => {
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
-    return bills
+    return filteredBillsValue
 
   }
 
