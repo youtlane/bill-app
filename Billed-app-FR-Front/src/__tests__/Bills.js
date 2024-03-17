4270cdc210805158a066fc1b0012a47bf3e1ffff
youtlane <youtlanehanane@gmail.com>
2024-03-17 15:14:01 +0100

    feat: add test to cover more than 80% of bills code

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,10 +5,13 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import userEvent from '@testing-library/user-event'
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -36,9 +39,83 @@ describe("Given I am connected as an employee", () => {
                     .map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
-      console.log('xmen', datesSorted);
-      console.log('xmen 2', dates);
       expect(dates).toEqual(datesSorted)
+    })
+    test("", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+      const dates = screen.getAllByText
+                    (/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
+                    .map(a => a.innerHTML)
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted)
+    })
+    test("Then modal should be displayed with correct content when eye icon is clicked", async () => {
+      const billUrl = "http://127.0.0.1:5500/src/assets/images/facturefreemobile.jpg"
+      const billsService = new Bills({
+        document, store: null, localStorage: window.localStorage
+      })
+      // Préparation du DOM avec une icône "œil"
+      document.body.innerHTML = BillsUI({ data: bills })
+      const iconEye = screen.getAllByTestId('icon-eye')[0]
+    
+      const handleShowModal = jest.fn((e) => billsService.handleClickIconEye(iconEye))
+
+      // Simulation d'un clic sur l'icône "œil"
+      iconEye.addEventListener('click', handleShowModal)
+      userEvent.click(iconEye)
+      expect(handleShowModal).toHaveBeenCalled()
+
+    
+      // Attente de l'affichage de la boîte modale
+      await waitFor(() => {
+        const modalFile = screen.queryByTestId('modale-file');
+        expect(modalFile).toBeDefined()
+      });
+    
+      // Récupération de la modal-body
+      const modalBody = screen.getByTestId('modale-file').querySelector('.modal-body')
+      const modalImage = modalBody.querySelector('img')
+
+      // Vérification du contenu de la boîte modale
+      expect(modalImage).toBeTruthy()
+      expect(modalImage.getAttribute('src')).toEqual(billUrl)
+    })
+    test("Then newBills page should be displayed when button newBills is clicked", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const billsService = new Bills({
+        document, onNavigate, localStorage: window.localStorage
+      })
+      // window.onNavigate(ROUTES_PATH.Bills)
+
+      document.body.innerHTML = BillsUI({ data: bills })
+      const buttonNewBill = screen.getAllByTestId('btn-new-bill')[0]
+    
+      const handleClickNewBill = jest.fn((e) => billsService.handleClickNewBill())
+
+      // Simulation d'un clic sur le boutton nouvelle note de frais
+      buttonNewBill.addEventListener('click', handleClickNewBill)
+      userEvent.click(buttonNewBill)
+      expect(handleClickNewBill).toHaveBeenCalled()
+
+      await waitFor(() => {
+        const formNewBill = screen.queryByTestId('form-new-bill');
+        expect(formNewBill).toBeDefined()
+      });
+    })
+
+    test("Then getBills is called ans should be return 4 bills", async () => {
+      const billsService = new Bills({
+        document, store: mockStore, localStorage: window.localStorage
+      })
+
+      const billsResult = await billsService.getBills();
+
+      // Vérifiez si le résultat est un tableau avec 4 éléments
+      expect(Array.isArray(billsResult)).toBe(true);
+      expect(billsResult.length).toBe(4);
     })
   })
 })
--- Billed-app-FR-Front/src/fixtures/bills.js
+++ Billed-app-FR-Front/src/fixtures/bills.js
@@ -1,7 +1,7 @@
 export const bills = [{
   "id": "47qAXb6fIm2zOKkLzMro",
   "vat": "80",
-  "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+  "fileUrl": "http://127.0.0.1:5500/src/assets/images/facturefreemobile.jpg",
   "status": "pending",
   "type": "Hôtel et logement",
   "commentary": "séminaire billed",
